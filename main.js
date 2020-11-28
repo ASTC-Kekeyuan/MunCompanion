f28ea5147273323f0624e649806f28bdf152c99d
lwzhenglittle <924695205@qq.com>
2020-11-28 15:59:59 +0800

    11.28

--- main.js
+++ main.js
@@ -3,14 +3,13 @@ const { app, BrowserWindow } = require('electron')
 function createWindow () {
   const win = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 250,
     webPreferences: {
       nodeIntegration: true
     }
   })
 
   win.loadFile('index.html')
-  win.webContents.openDevTools()
 }
 
 app.whenReady().then(createWindow)
